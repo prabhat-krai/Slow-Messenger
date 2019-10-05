010fe65c308bab5abc4677402248a9f59d810c86
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-06 00:04:54 +0530

    Basic Static page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Checking that it is running"
+    return "Searching for restaurants"
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
 
 if (__name__ == '__main__'):
     app.run(debug = True)
