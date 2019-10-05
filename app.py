6d6d2a3209bd000250ebff96651a25a20be2047b
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-05 23:51:43 +0530

    Basic Structure

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Checking that it is running"
+
+if (__name__ == '__main__'):
+    app.run(debug = True)
+
+
 
