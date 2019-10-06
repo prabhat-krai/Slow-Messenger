b78eef72a014bdaeea568dbae92af1de97546e58
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-06 17:36:20 +0530

    secret key added

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+import os
+
+key = os.urandom(33)
+
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'rememberthis'
+    SECRET_KEY = key
     SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
 
 class Dev(BaseConfig):
     DEBUG = True
-    
+
 class Prod(BaseConfig):
     DEBUG = False
