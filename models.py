dad30cfdc55023caa925f6ed8133b20d381f73dc
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-07 01:18:43 +0530

    FAILED TO ADD POSTGRESQL

--- models.py
+++ models.py
@@ -1,11 +1,15 @@
 from app import db
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 class Blogpost(db.Model):
 
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String, primary_key = False)
     description = db.Column(db.String, primary_key = False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
 
     def __init__(self, title, description):
         self.title = title
@@ -13,3 +17,22 @@ class Blogpost(db.Model):
 
     def __repr__(self):
         return '<{}>, <{}>'.format(self.title, self.description)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<name {}'.format(self.name)
