088f84a8a9998c8e75423481e1514d721500c8bc
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-07 17:36:28 +0530

    passwords are hashed

---
+++ create_users.py
@@ -0,0 +1,10 @@
+from app import db
+from models import User
+
+db.create_all()
+
+db.session.add(User("master", "mast@min.com", "password"))
+db.session.add(User("Pawan", "air@min.com", "water"))
+db.session.add(User("Phoenix", "fire@min.com", "ice"))
+
+db.session.commit()
